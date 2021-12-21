cd132f7a5920739825401b134303bd273805552d
Ivan <butrynio@gmail.com>
2021-12-21 17:53:34 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+
+
+
+
+        public static void main(String[] args) {
+            int x = 10_000, y = 20_000, a = 30_000, w = 40_000, e = 50_000, b = 60_0000;
+
+            System.out.println(x + y - a / 2 * (x + w));
+            System.out.println(a * (x + x) / y - a * 3);
+            System.out.println(a + b / x - y);
+            System.out.println(x - e / 2 % e + 1);
+
+        }
+    }
